ff8938b8fb40bdd8cfb529c1fd0d1b1a01c46293
Ani <anirbansarkar208@gmail.com>
2026-09-24 07:16:18 +0530

    temporary addition of stream parsing, before moving to stream parser

--- include/text_stream.h
+++ include/text_stream.h
@@ -12,12 +12,18 @@
   typedef int epoll_t;
 #endif
 
+#if !defined (_ANI_TEXT_STREAM_H )
 
-#if !defined (_ANI_TEXT_STREAM_H)
+constexpr std::string_view blue_dark  = "\033[38;2;30;60;120m";
+constexpr std::string_view blue       = "\033[38;2;60;120;220m";
+constexpr std::string_view blue_pro = "\033[38;2;70;130;240m";
+constexpr std::string_view blue_bright = "\033[38;2;80;160;255m";
+constexpr std::string_view reset_color = "\033[39m";
 
 class context_stream {
 public:
-  void consume( ); 
+  void consume( );
+  void consume_even_better( );
   void push( std::string_view );
 public:
   context_stream( );
@@ -29,6 +35,8 @@ public:
   epoll_event r_evs[5];
   std::mutex mtx;
   int event_fd;
+  bool styling;
+  bool pending_char;
 };
 
 #define _ANI_TEXT_STREAM_H
--- src/text_stream.cpp
+++ src/text_stream.cpp
@@ -12,13 +12,102 @@ void context_stream::consume( ) {
   epoll_wait( epoll, r_evs, 5, -1 );
   eventfd_t value;
   eventfd_read( event_fd, &value );
+  size_t ptr = 0, end = 0;
+  std::string_view data;
+  bool styling = false;
   while ( value > 0 ) {
-    {
-      std::lock_guard lock( mtx );
-      std::cout << stream.front( );
-      stream.pop( );
+    auto& chunk = stream.front( );
+    ptr = chunk.find( "**", 0 );
+    if ( ptr == std::string::npos ) {
+      {
+        std::lock_guard lock( mtx );
+        std::cout.write( chunk.data( ), chunk.size( ) );
+        stream.pop( );
+      }
+    }
+    else {
+      if ( styling ) {
+        std::cout << "[reset]";
+        chunk.replace( ptr, 2, reset_color.data( ), reset_color.size( ) );
+        styling = false;
+      }
+      else {
+        std::cout << "[blue]";
+        chunk.replace( ptr, 2, blue_dark.data( ), blue_dark.size( ) );
+        styling = true;
+      }
+      {
+        std::lock_guard lock( mtx );
+        std::cout.write( chunk.data( ), chunk.size( ) );
+        stream.pop( );
+      }
+      ptr += 2;
     }
     value--;
+  }
+}
+
+// needs correction, kinda sexy but ass
+void context_stream::consume_even_better( ) {
+  epoll_wait( epoll, r_evs, 5, -1 );
+  eventfd_t value;
+  eventfd_read( event_fd, &value );
+  size_t ptr = 0, end = 0;
+  while ( value-- ) {
+    auto& chunk = stream.front( );
+    size_t pos = 0;
+    if ( pending_char ) {
+      if ( !chunk.empty( ) && chunk[0] == '*' ) {
+        if ( styling ) {
+          std::cout.write( reset_color.data( ), reset_color.size( ) );
+          styling = false;
+        }
+        else {
+          std::cout.write( blue_dark.data( ), blue_dark.size( ) );
+          styling = true;
+        }
+        pos = 1;
+        pending_char = false;
+      }
+      else {
+        std::cout.put('*');
+        pending_char = false;
+      }
+    }
+    while ( pos < chunk.size( ) ) {
+      size_t ptr = chunk.find( "**", pos );
+      if ( ptr == std::string::npos ) {
+        if ( chunk.back( ) == '*' ) {
+          if ( pos < chunk.size( ) - 1 ) {
+            const auto data = chunk.subview( pos, chunk.size( ) - pos - 1 );
+            std::cout.write( data.data( ), data.size( ) );
+          }
+          pending_char = true;
+        }
+        else {
+          const auto data = chunk.subview( pos );
+          std::cout.write( data.data( ), data.size( ) );
+        }
+        break;
+      }
+
+      if ( ptr > pos ) {
+        const auto data = chunk.subview( pos, ptr - pos );
+        std::cout.write( data.data( ), data.size( ) );
+      }
+
+      if ( styling ) {
+        std::cout.write( reset_color.data( ), reset_color.size( ) );
+        styling = false;
+      }
+      else {
+        std::cout.write( blue_pro.data( ), blue_pro.size( ) );
+        styling = true;
+      }
+
+      pos = ptr + 2;
+    }
+    stream.pop( );
   }
 }
 
